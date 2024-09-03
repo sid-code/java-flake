caaa97ed04f8e8daab8cd74c2843fe84a8ed6a34
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2024-09-03 08:56:42 -0700

    add some options to mkJavaMavenShell

--- flake.nix
+++ flake.nix
@@ -2,31 +2,41 @@
   outputs = _: {
     lib = {
       mkJavaMavenShell =
-        { pkgs, fetchedMavenDeps }:
+        {
+          pkgs,
+          fetchedMavenDeps,
+          useOutsideDirectory ? false,
+          jdk ? pkgs.jdk11_headless,
+        }:
 
         pkgs.mkShell {
-          packages = with pkgs; [
-            jdk11_headless
-            jdt-language-server
-            maven
+          packages = [
+            jdk
+            pkgs.jdt-language-server
+            pkgs.maven
           ];
 
-          # Initialize .jdtls directory with stuff that jdt-language-server
-          # needs. It should probably be added to .git/info/exclude (or
-          # equivalent) of the project
-          shellHook = ''
-            export TMPDIR=$PWD/.jdtls
+          # Initialize temporary directory with stuff that
+          # jdt-language-server needs. If `useOutsideDirectory` is
+          # false, it should probably be added to .git/info/exclude
+          # (or equivalent) of the project
+          shellHook =
+            let
+              tmpDir = if useOutsideDirectory then ''/tmp/javalsp/''${PWD//\//_}'' else "$PWD/.jdtls";
+            in
+            ''
+              export TMPDIR=${tmpDir}
 
-            rm -rf $TMPDIR/
-            mkdir -p $TMPDIR/{maven,config,workspace}
-            cp -dpR ${fetchedMavenDeps}/.m2 $TMPDIR/maven/
-            cp -dpR ${pkgs.jdt-language-server}/share/java/jdtls/config_linux $TMPDIR/config/
-            chmod +w -R $TMPDIR
+              rm -rf $TMPDIR/
+              mkdir -p $TMPDIR/{maven,config,workspace}
+              cp -dpR ${fetchedMavenDeps}/.m2 $TMPDIR/maven/
+              cp -dpR ${pkgs.jdt-language-server}/share/java/jdtls/config_linux $TMPDIR/config/
+              chmod +w -R $TMPDIR
 
-            export MAVEN_OPTS="-Dmaven.repo.local=$TMPDIR/maven/.m2"
-            export JDTLS_CONFIG="$TMPDIR/config/config_linux"
-            export JDTLS_WORKSPACE="$TMPDIR/workspace"
-          '';
+              export MAVEN_OPTS="-Dmaven.repo.local=$TMPDIR/maven/.m2"
+              export JDTLS_CONFIG="$TMPDIR/config/config_linux"
+              export JDTLS_WORKSPACE="$TMPDIR/workspace"
+            '';
         };
     };
   };
